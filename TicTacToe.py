31ed93478d88a72eb6b193eb095f37205341d5db
Camilo Gomez <camilogomezmusic@gmail.com>
2021-09-08 14:14:21 -0400

    Refactor all logic into TicTacToe class

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,6 +1,5 @@
 import pygame
 
-state = [0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0]
 
 class TicTacToe:
     board = pygame.image.load('tic_tac_toe.png')    
@@ -15,11 +14,19 @@ class TicTacToe:
         self.state = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
         self.o_win = False
         self.x_win = False
+        self.turn = 0
 
-    # These functions return 0, 1 or 2
+    def move(self, pos):
+        i, j = pos
+        self.state[i][j] = 2 if self.turn == 0 else 1
+        self.next_turn()
+
+    def next_turn(self):
+        self.turn = (self.turn + 1) % 2
+
     def check_horizontal(self):
+        # Can be refactored
         for i in self.state:
-            print(i)
             if all(element == 1 for element in i):
                 return 1
             elif all(element == 2 for element in i):
@@ -27,15 +34,43 @@ class TicTacToe:
         return 0
     
     def check_vertical(self):
-        pass
-
+        for i in range(len(self.state)):
+            col = [row[i] for row in self.state]
+            if all(element == col[0] for element in col):
+                return col[0]
+        return 0
+            
     def check_diagonal(self):
-        pass
+        center = self.state[1][1]
+        if self.state[0][0] == self.state[2][2] == center or self.state[2][0] == self.state[0][2] == center :
+            return center
+        return 0
+
+    def check_winner(self):
+        if self.check_vertical():
+            return self.check_vertical()
+        elif self.check_horizontal():
+            return self.check_horizontal()
+        elif self.check_diagonal():
+            return self.check_diagonal()
+        
+        return 0
     
     def display(self, screen):
         screen.blit(self.board, (0,0))
         self.update_board(screen)
-             
+        
+        if self.turn == 1:
+            screen.blit(self.ind_x, (0, 300))
+        elif self.turn == 0:
+            screen.blit(self.ind_o, (0, 300))
+
+        winner = self.check_winner()
+        if winner == 1:
+            screen.blit(self.xwin, (0,0))
+        elif winner == 2:
+            screen.blit(self.owin, (0,0))
+            
     def update_board(self, screen):
         for i, line in enumerate(self.state):
             for j, cell in enumerate(line):
@@ -43,91 +78,3 @@ class TicTacToe:
                     screen.blit(self.img_x, (j*100, i*100))
                 elif cell == 2:
                     screen.blit(self.img_o, (j*100, i*100))
-
-
-        # Display the state
-        if state[0] == 1:
-            screen.blit(self.img_x, (0, 0))
-        if state[1] == 1:
-            screen.blit(self.img_x, (100, 0))
-        if state[2] == 1:
-            screen.blit(self.img_x, (200, 0))
-        if state[3] == 1:
-            screen.blit(self.img_x, (0, 100))
-        if state[4] == 1:
-            screen.blit(self.img_x, (100, 100))
-        if state[5] == 1:
-            screen.blit(self.img_x, (200, 100))
-        if state[6] == 1:
-            screen.blit(self.img_x, (0, 200))
-        if state[7] == 1:
-            screen.blit(self.img_x, (100, 200))
-        if state[8] == 1:
-            screen.blit(self.img_x, (200, 200))
-        if state[0] == 2:
-            screen.blit(self.img_o, (0, 0))
-        if state[1] == 2:
-            screen.blit(self.img_o, (100, 0))
-        if state[2] == 2:
-            screen.blit(self.img_o, (200, 0))
-        if state[3] == 2:
-            screen.blit(self.img_o, (0, 100))
-        if state[4] == 2:
-            screen.blit(self.img_o, (100, 100))
-        if state[5] == 2:
-            screen.blit(self.img_o, (200, 100))
-        if state[6] == 2:
-            screen.blit(self.img_o, (0, 200))
-        if state[7] == 2:
-            screen.blit(self.img_o, (100, 200))
-        if state[8] == 2:
-            screen.blit(self.img_o, (200, 200))
-
-    def if_o_win(self):
-        if self.board[0] == 2 and self.board[3] == 2 and self.board[6] == 2:
-            self.o_win = True
-        elif self.board[1] == 2 and self.board[4] == 2 and self.board[7] == 2:
-            self.o_win = True
-        elif self.board[2] == 2 and self.board[5] == 2 and self.board[8] == 2:
-            self.o_win = True
-        elif self.board[0] == 2 and self.board[1] == 2 and self.board[2] == 2:
-            self.o_win = True
-        elif self.board[3] == 2 and self.board[4] == 2 and self.board[5] == 2:
-            self.o_win = True
-        elif self.board[6] == 2 and self.board[7] == 2 and self.board[8] == 2:
-            self.o_win = True
-        elif self.board[0] == 2 and self.board[4] == 2 and self.board[8] == 2:
-            self.o_win = True
-        elif self.board[2] == 2 and self.board[4] == 2 and self.board[6] == 2:
-            self.o_win = True
-        elif self.board[0] == 2 and self.board[2] == 2 and self.board[6] == 2:
-            self.o_win = True
-        elif self.board[0] == 2 and self.board[2] == 2 and self.board[6] == 2:
-            self.o_win = True
-        elif self.board[0] == 2 and self.board[2] == 2 and self.board[6] == 2:
-            self.o_win = True
-
-    def if_x_win(self):
-        if self.board[0] == 1 and self.board[3] == 1 and self.board[6] == 1:
-            self.x_win = True
-        elif self.board[1] == 1 and self.board[4] == 1 and self.board[7] == 1:
-            self.x_win = True
-        elif self.board[2] == 1 and self.board[5] == 1 and self.board[8] == 1:
-            self.x_win = True
-        elif self.board[0] == 1 and self.board[1] == 1 and self.board[2] == 1:
-            self.x_win = True
-        elif self.board[3] == 1 and self.board[4] == 1 and self.board[5] == 1:
-            self.x_win = True
-        elif self.board[6] == 1 and self.board[7] == 1 and self.board[8] == 1:
-            self.x_win = True
-        elif self.board[0] == 1 and self.board[4] == 1 and self.board[8] == 1:
-            self.x_win = True
-        elif self.board[2] == 1 and self.board[4] == 1 and self.board[6] == 1:
-            self.x_win = True
-        elif self.board[0] == 1 and self.board[2] == 1 and self.board[6] == 1:
-            self.x_win = True
-        elif self.board[0] == 1 and self.board[2] == 1 and self.board[6] == 1:
-            self.x_win = True
-        elif self.board[0] == 1 and self.board[2] == 1 and self.board[6] == 1:
-            self.x_win = True
-
